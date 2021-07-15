6c2811c660b8e6bfa8549bb6ed03a41121634159
yandeu <20306025+yandeu@users.noreply.github.com>
2021-07-15 15:48:57 +0200

    Added some colors

--- docker/run.cjs
+++ docker/run.cjs
@@ -19,18 +19,26 @@ const getArgs = async () => {
       break
   }
 
-  console.log(`Will start PHP server on "http://localhost:3200/"`)
-  await pause(2000)
+  const blue = t => `\u001b[34m${t}\u001b[39m`
+  const cyan = t => `\u001b[36m${t}\u001b[39m`
+  const brightBlack = t => `\u001b[90m${t}\u001b[39m`
+  const arrow = blue('⠕ ')
 
-  console.log(`Running: "docker-compose ${args.join(' ')}"`)
-  await pause(500)
+  console.log(`${arrow} Will start PHP server on ${cyan('http://localhost:3200/')}`)
+  console.log('')
+  await pause(3000)
+
+  const cmd = brightBlack(`docker-compose ${args.join(' ')}`)
+  console.log(`${arrow} Running: ${cmd}`)
+  console.log('')
+  await pause(1000)
 
   return args
 }
 
 const main = async () => {
   const args = await getArgs()
-  
+
   process.on('SIGINT', function () {
     console.log('')
     console.log('> shutdown')
